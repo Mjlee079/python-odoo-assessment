908de92f77cbf8207dfa57506ff50f426c571a1a
Mulaudzi <114157158+Mjlee079@users.noreply.github.com>
2024-05-10 19:01:13 -0700

    Delete test_website directory

--- test_website/controllers/main.py
+++
@@ -1,7 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class WebsiteController(http.Controller):
-    @http.route('/custom_website/', auth='public', website=True)
-    def index(self, **kw):
-        return http.request.render('custom_website.index', {})
--- test_website/models/custome_website.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Website(models.Model):
-    _name = 'custom.website'
-    _description = 'Custom Website Model'
-
-    name = fields.Char(string='Name')
-    description = fields.Text(string='Description')
